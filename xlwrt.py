1ce8f3ea8be974d84c91d5e943f602504fa3716f
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-04-22 10:02:06 +0900

    enrich

--- xlwrt.py
+++ xlwrt.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
+def extract_col(cell_idx):
+    import re
+    return re.match(r"[A-Za-z]+", cell_idx).group()
+def extract_row(cell_idx):
+    import re
+    return re.match(r"\d+", cell_idx).group()
+
 def to_row_num(row_alpha):
     i =  0
     row = 0
@@ -19,19 +25,24 @@ def to_row_alph(row_num):
     l -= second * 26
     return ''.join(map(lambda n: "" if n == 0 else chr(n + ord('A') - 1), [third, second, l]))
 
-
 def filled_lines(alpha, size):
     a = to_row_num(alpha)
     return [to_row_alph(c+a) for c in range(size)]
 
-#print (to_row_alph(to_row_num('Z')))
-#print (filled_lines('Z', 3))
+def args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Writes csv read from standard input to a excel file.')
+    parser.add_argument('--top_left_cell','-c', nargs=1, metavar='A1')
+    parser.add_argument('--xlsx','-x', nargs=1, metavar='excel_file.xlsx')
+    parser.add_argument('--sheet','-s', nargs=1, metavar='sheet_name')
+    return parser.parse_args()
 
 # http://openpyxl.readthedocs.org/en/default/tutorial.html
+if __name__ == '__main__':
+    import sys
+    import csv
+    lines = [ l for l in csv.reader(sys.stdin.readlines())]
+    row_siz=len(lines)
+    col_siz=len(lines[0])
 
-import sys
-import csv
-lines = [ l for l in csv.reader(sys.stdin.readlines())]
-row_siz=len(lines)
-col_siz=len(lines[0])
 
